a44f74bfa43a194cd5885d1dd8759864e589df60
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2026-03-13 18:48:59 +0530

    change cookieOptions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -94,6 +94,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const cookieOptions = {
         httpOnly: true,
         secure: true,
+        sameSite: "none",
     };
 
     return res
